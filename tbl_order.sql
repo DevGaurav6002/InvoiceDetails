14806986a808ff32aa54a296c450f09c85c46c55
DevGaurav6002 <83692986+DevGaurav6002@users.noreply.github.com>
2022-01-05 05:09:06 +0530

    orders table

---
+++ tbl_order.sql
@@ -0,0 +1,76 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 05, 2022 at 12:31 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `testing4`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_order`
+--
+
+CREATE TABLE `tbl_order` (
+  `order_id` int(11) NOT NULL,
+  `order_no` varchar(50) NOT NULL,
+  `order_date` date NOT NULL,
+  `order_receiver_name` varchar(250) NOT NULL,
+  `order_receiver_address` text NOT NULL,
+  `order_total_before_tax` decimal(10,2) NOT NULL,
+  `order_total_tax1` decimal(10,2) NOT NULL,
+  `order_total_tax2` decimal(10,2) NOT NULL,
+  `order_total_tax3` decimal(10,2) NOT NULL,
+  `order_total_tax` decimal(10,2) NOT NULL,
+  `order_total_after_tax` decimal(10,2) NOT NULL,
+  `order_datetime` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_order`
+--
+
+INSERT INTO `tbl_order` (`order_id`, `order_no`, `order_date`, `order_receiver_name`, `order_receiver_address`, `order_total_before_tax`, `order_total_tax1`, `order_total_tax2`, `order_total_tax3`, `order_total_tax`, `order_total_after_tax`, `order_datetime`) VALUES
+(1, '1100', '2022-01-04', 'John Smilga', 'USA', '0.00', '0.00', '0.00', '0.00', '0.00', '4999.00', '2022-01-04 00:00:00'),
+(2, '1500', '2022-01-04', 'derek', 'USA', '0.00', '0.00', '0.00', '0.00', '0.00', '8000.00', '2022-01-04 00:00:00');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_order`
+--
+ALTER TABLE `tbl_order`
+  ADD PRIMARY KEY (`order_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_order`
+--
+ALTER TABLE `tbl_order`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
